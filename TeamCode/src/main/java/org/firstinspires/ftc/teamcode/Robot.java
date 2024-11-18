2f97118aaaae2ba5302fa45b2d06c4dce62846fb
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2024-11-18 09:33:42 -0700

    Code for charlie so that he can test the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -6,14 +6,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp
 public class Robot extends OpMode {
+    private final Arm arm = new Arm();
+
 
     @Override
     public void init() {
-
+        this.arm.init(hardwareMap);
     }
 
     @Override
     public void loop() {
-
+        this.arm.runArmMotor(gamepad1.left_stick_x);
     }
 }
