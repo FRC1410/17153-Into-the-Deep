894b583e01fd83345a44fbe560ab743bd34f742f
Calstar9000 <Calstar9000+github@gmail.com>
2025-01-11 14:07:35 -0700

    auto prototype

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/ScoreOneLeft.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Actions.Lower;
+import org.firstinspires.ftc.teamcode.Actions.Raise;
+import org.firstinspires.ftc.teamcode.Subsystem.Shoulder;
+import org.firstinspires.ftc.teamcode.Subsystem.Claw;
+import org.firstinspires.ftc.teamcode.Subsystem.Drivetrain;
+import org.firstinspires.ftc.teamcode.Subsystem.LinearSlide;
+import org.firstinspires.ftc.teamcode.Subsystem.Wrist;
+import org.firstinspires.ftc.teamcode.Auto.*;
+import org.firstinspires.ftc.teamcode.Subsystem.Shoulder;
+import org.firstinspires.ftc.teamcode.Util.RobotStates;
+import org.firstinspires.ftc.teamcode.Subsystem.LinearSlide;
+import org.firstinspires.ftc.teamcode.Subsystem.Wrist;
+
+
+@Autonomous(name="Robot: Score one on left (TESTING ONLY)", group="Auto")
+
+public class ScoreOneLeft extends LinearOpMode {
+
+    private final AutoDriveTrain drivetrain = new AutoDriveTrain();
+    private final Shoulder shoulder = new Shoulder();
+    private final LinearSlide linearSlide = new LinearSlide();
+    private final Claw claw = new Claw();
+    private final Wrist wrist = new Wrist();
+
+    private final Raise raiseFullCommand = new Raise(linearSlide, shoulder, wrist);
+    private final Lower lowerCommand = new Lower(linearSlide, shoulder, wrist);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        this.drivetrain.init(this.hardwareMap);
+        this.shoulder.init(this.hardwareMap);
+        this.linearSlide.init(this.hardwareMap);
+        this.claw.init(this.hardwareMap);
+        this.wrist.init(this.hardwareMap);
+
+        waitForStart();
+
+
+//        this.claw.setClawPos(RobotStates.Claw.CLOSED);
+//        drivetrain.drive(0,-0.2,-0.8);
+        Thread.sleep(500);
+        drivetrain.drive(0,0,0);
+        this.shoulder.setState(RobotStates.Arm.UP);
+        Thread.sleep(100);
+        this.linearSlide.setState(RobotStates.LinearSlide.HIGH_SCORE);
+        Thread.sleep(300);
+        this.wrist.setState(RobotStates.Wrist.SCORE);
+        Thread.sleep(200);
+//        this.claw.setClawPos(RobotStates.Claw.OPEN);
+//        Thread.sleep(200);
+        this.linearSlide.setState(RobotStates.LinearSlide.START_POS);
+        Thread.sleep(100);
+        this.shoulder.setState(RobotStates.Arm.DOWN);
+    }
+}
